9791eb1ae97acf35734b8265f56b183166984459
hanwei <545717582@qq.com>
2020-04-01 21:21:23 +0800

    首次提交

--- src/com/han/demo/ByteCodeExample.java
+++ src/com/han/demo/ByteCodeExample.java
@@ -2,11 +2,12 @@ package com.han.demo;
 
 public class ByteCodeExample {
     public static void main(String[] args) {
-        int i=2,j=6;
-            i++;
-            j++;
-            System.out.println(i);
-            System.out.println(j);
+        int i = 2, j = 6;
+        i++;
+        j++;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(j);
 
     }
 }
